980a58fbac87c9b1eb7f7ea5335d2efe37ba9c5f
Val Saven <val.saven@gmail.com>
2018-05-15 01:12:23 +0300

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Fractal/>
+  </div>
+</template>
+
+<script>
+import Fractal from './components/Fractal.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Fractal,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
